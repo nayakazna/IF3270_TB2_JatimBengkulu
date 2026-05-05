b3e37d5f6f50263dba754af41ea4e100ab2ed315
Muhammad Adha Ridwan <140234882+adharidwan@users.noreply.github.com>
2026-05-05 15:05:17 +0700

    feat: convolution layer

---
+++ src/cnn/layers/conv2d.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+class Conv2D:
+    def __init__(self, weight, bias, stride = 1, pad = 0, activation=None) -> None:
+        self.weight = weight
+        self.bias = bias
+        self.stride = stride
+        self.pad = pad
+        self.activation = activation
+
+    def forward(self, x):
+        # (Height, Width, Channel)
+        if x.ndim == 3:
+            return self._forward(x)
+        # (Image Number, Height, Width, Channel)
+        elif x.ndim == 4:
+            return np.stack([self._forward(img) for img in x], axis=0)
+        
+
+    def _forward(self, x):
+        H, W, C_in = x.shape
+        kH, kW, C_weight, C_out = self.weight.shape
+
+        if C_in != C_weight:
+            raise ValueError(f"Input channel {C_in} != weight channel {C_weight}")
+
+        if self.pad > 0:
+            x = np.pad(
+                x,
+                (
+                    (self.pad, self.pad),
+                    (self.pad, self.pad),
+                    (0, 0),
+                ),
+                mode="constant",
+            )
+
+        H_pad, W_pad, _ = x.shape
+
+        H_out = (H_pad - kH) // self.stride + 1
+        W_out = (W_pad - kW) // self.stride + 1
+
+        out = np.zeros((H_out, W_out, C_out), dtype=np.float32)
+
+        for i in range(H_out):
+            for j in range(W_out):
+                h_start = i * self.stride
+                h_end = h_start + kH
+                w_start = j * self.stride
+                w_end = w_start + kW
+
+                patch = x[h_start:h_end, w_start:w_end, :]
+
+                for c in range(C_out):
+                    out[i, j, c] = np.sum(
+                        patch * self.weight[:, :, :, c]
+                    ) + self.bias[c]
+
+        if self.activation is not None:
+            out = self.activation(out)
+
+        return out
---
+++ src/cnn/layers/locallyconnected2d.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+
+class LocallyConnected2D:
+    def __init__(self, weight, bias, kernel_size, stride=1, pad=0, activation=None):
+        self.weight = weight
+        self.bias = bias
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.pad = pad
+        self.activation = activation
+
+    def forward(self, x):
+               
+        if x.ndim == 3:
+            return self._forward_single(x)
+
+        if x.ndim == 4:
+            return np.stack([self._forward_single(img) for img in x], axis=0)
+
+        raise ValueError("Input must have shape (H, W, C) or (N, H, W, C)")
+
+    def _forward_single(self, x):
+        H, W, C_in = x.shape
+        kH, kW = self.kernel_size
+
+        if self.pad > 0:
+            x = np.pad(
+                x,
+                (
+                    (self.pad, self.pad),
+                    (self.pad, self.pad),
+                    (0, 0),
+                ),
+                mode="constant",
+            )
+
+        H_pad, W_pad, _ = x.shape
+
+        H_out = (H_pad - kH) // self.stride + 1
+        W_out = (W_pad - kW) // self.stride + 1
+
+        num_locations = H_out * W_out
+        patch_size = kH * kW * C_in
+
+        if self.weight.shape[0] != num_locations:
+            raise ValueError(
+                f"Weight locations {self.weight.shape[0]} != output locations {num_locations}"
+            )
+
+        if self.weight.shape[1] != patch_size:
+            raise ValueError(
+                f"Weight patch size {self.weight.shape[1]} != expected patch size {patch_size}"
+            )
+
+        C_out = self.weight.shape[2]
+        out = np.zeros((H_out, W_out, C_out), dtype=np.float32)
+
+        location_idx = 0
+
+        for i in range(H_out):
+            for j in range(W_out):
+                h_start = i * self.stride
+                h_end = h_start + kH
+                w_start = j * self.stride
+                w_end = w_start + kW
+
+                patch = x[h_start:h_end, w_start:w_end, :]
+                patch_flat = patch.reshape(-1)
+
+                local_weight = self.weight[location_idx]
+
+                if self.bias.ndim == 2:
+                    local_bias = self.bias[location_idx]
+                else:
+                    local_bias = self.bias
+
+                out[i, j, :] = patch_flat @ local_weight + local_bias
+
+                location_idx += 1
+
+        if self.activation is not None:
+            out = self.activation(out)
+
+        return out
